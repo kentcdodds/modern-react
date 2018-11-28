8d2761b99534fc9791f13576f560f57d06ba0562
Kent C. Dodds <kent@doddsfamily.us>
2018-11-28 11:37:51 -0800

    fix feedback link

--- src/__tests__/04.js
+++ src/__tests__/04.js
@@ -27,7 +27,7 @@ test('Usage works', () => {
 // 3. Change submitted from `false` to `true`
 // 4. And you're all done!
 /*
-http://ws.kcd.im/?ws=modern%20react&e=01&em=
+http://ws.kcd.im/?ws=modern%20react&e=04&em=
 */
 test.skip('I submitted my elaboration and feedback', () => {
   const submitted = false // change this when you've submitted!
